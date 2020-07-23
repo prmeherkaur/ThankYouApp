a1d3ff644cfdae259893158e172e4fce741471f8
Prmeher Kaur <prmeherkaur@gmail.com>
2020-07-23 16:10:30 +0530

    Updates

---
+++ precache-manifest.dca759cd2be0ae84e28324d7a4bf101b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4351d76da5161acc516ee2d6e28db217",
+    "url": "/ThankYouApp/index.html"
+  },
+  {
+    "revision": "7401e627b00098f6e07e",
+    "url": "/ThankYouApp/static/css/main.4d633967.chunk.css"
+  },
+  {
+    "revision": "38b7311bafe37eaa43a3",
+    "url": "/ThankYouApp/static/js/2.aedae7d5.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/ThankYouApp/static/js/2.aedae7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7401e627b00098f6e07e",
+    "url": "/ThankYouApp/static/js/main.326456a2.chunk.js"
+  },
+  {
+    "revision": "fb13dac5c143b84db6da",
+    "url": "/ThankYouApp/static/js/runtime-main.cd3e7eac.js"
+  },
+  {
+    "revision": "5facc3f9e5ae8755b0752fabeb84ac18",
+    "url": "/ThankYouApp/static/media/Devashish.5facc3f9.png"
+  },
+  {
+    "revision": "abd0de9c73a186a85c348957a1b11308",
+    "url": "/ThankYouApp/static/media/Prmeher.abd0de9c.jpg"
+  },
+  {
+    "revision": "eb8df355a20d0c306630adabadcbf0ee",
+    "url": "/ThankYouApp/static/media/Sahil.eb8df355.png"
+  },
+  {
+    "revision": "b02c8f5637a256417418d9bf66fcb5e5",
+    "url": "/ThankYouApp/static/media/Sumanasa.b02c8f56.jpg"
+  }
+]);
